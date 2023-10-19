c6f987f1eb6a085cde70e0b16ef228c1423cc7e2
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2023-10-19 11:30:46 +0200

    fragmento AcercaDe inflado

--- android/InazumaWiki/app/src/main/java/com/afundacion/inazumawiki/acercade/FragmentAcercaDe.java
+++ android/InazumaWiki/app/src/main/java/com/afundacion/inazumawiki/acercade/FragmentAcercaDe.java
@@ -1,13 +1,18 @@
 package com.afundacion.inazumawiki.acercade;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.afundacion.myaplication.R;
 
 
@@ -38,4 +43,21 @@ public class FragmentAcercaDe extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_acerca_de, container, false);
     }
+
+    @SuppressLint("MissingInflatedId")
+    public View onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState, LayoutInflater inflater, ViewGroup container) {
+        super.onViewCreated(view, savedInstanceState);
+
+        View layout = inflater.inflate(R.layout.fragment_acerca_de, container, false);
+
+        if (getArguments() != null) {
+            String text = getString(getArguments().getInt(TEXT_ID));
+            ((TextView) layout.findViewById(R.id.TextoAcercaDe)).setText(text);
+        } else {
+            throw new IllegalArgumentException("Argument " + TEXT_ID + " is mandatory");
+        }
+
+        return layout;
+    }
+
 }
