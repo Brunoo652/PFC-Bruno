80bb37cb93cd4a7b32e4b06f42a9c97f4b8b739a
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2023-10-23 13:36:49 +0200

    version 1.1 del drawer layout, modo noche/dia con problemas (parapadea la pantalla cuando pones el modo oscuro)

--- android/InazumaWiki/app/src/main/java/com/afundacion/inazumawiki/main/MainActivity.java
+++ android/InazumaWiki/app/src/main/java/com/afundacion/inazumawiki/main/MainActivity.java
@@ -5,7 +5,6 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
-
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -15,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import com.afundacion.inazumawiki.acercade.FragmentAcercaDe;
 import com.afundacion.inazumawiki.clubes.FragmentBuscarClubesNombre;
 import com.afundacion.inazumawiki.favoritos.FragmentListaFavoritos;
+import com.afundacion.inazumawiki.home.HomeFragment;
 import com.afundacion.inazumawiki.jugadores.FragmentBuscarJugadoresNombre;
 import com.afundacion.inazumawiki.objetos.FragmentBuscarObjetosNombre;
 import com.afundacion.inazumawiki.opciones.FragmentOpciones;
@@ -22,12 +22,10 @@ import com.afundacion.inazumawiki.st.FragmentBuscarSTNombre;
 import com.afundacion.myaplication.R;
 import com.google.android.material.navigation.NavigationView;
 
-
-
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout drawerLayout;
 
-    Fragment selectedFragment = new FragmentBuscarJugadoresNombre();
+    private DrawerLayout drawerLayout;
+    private Fragment selectedFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,58 +35,52 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //Funcionamiento del NavigationView
-        NavigationView navigationView = findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+        selectedFragment = new HomeFragment();
 
-        //Funcionamiento del DrawerLayout
+        // Funcionamiento del NavigationView
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        // Funcionamiento del DrawerLayout
         drawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
 
-                int id=item.getItemId();
-                if (id==R.id.BuscadorJugadorNombre){
-                    selectedFragment=new FragmentBuscarJugadoresNombre();
-                }
-                if (item.getItemId() == R.id.BuscadorClubesNombre) {
-                    selectedFragment=new FragmentBuscarClubesNombre();
-                }
-                if (item.getItemId() == R.id.BuscadorClubesNombre) {
-                    selectedFragment=new FragmentBuscarClubesNombre();
-                }
-                if (item.getItemId() == R.id.BuscadorobjetosNombre) {
-                    selectedFragment=new FragmentBuscarObjetosNombre();
-                }
-                if (item.getItemId() == R.id.BuscadorSTNombre) {
-                    selectedFragment=new FragmentBuscarSTNombre();
-                }
-                if (item.getItemId() == R.id.ListaFavoritos) {
-                    selectedFragment=new FragmentListaFavoritos();
-                }
-                if (item.getItemId() == R.id.AcercaDe) {
-                    selectedFragment= new FragmentAcercaDe();
-                }
-                if (item.getItemId() == R.id.Opciones) {
-                    selectedFragment=new FragmentOpciones();
+                if (id == R.id.BuscadorJugadorNombre) {
+                    selectedFragment = new FragmentBuscarJugadoresNombre();
+                } else if (id == R.id.BuscadorClubesNombre) {
+                    selectedFragment = new FragmentBuscarClubesNombre();
+                } else if (id == R.id.BuscadorobjetosNombre) {
+                    selectedFragment = new FragmentBuscarObjetosNombre();
+                } else if (id == R.id.BuscadorSTNombre) {
+                    selectedFragment = new FragmentBuscarSTNombre();
+                } else if (id == R.id.ListaFavoritos) {
+                    selectedFragment = new FragmentListaFavoritos();
+                } else if (id == R.id.AcercaDe) {
+                    selectedFragment = new FragmentAcercaDe();
+                } else if (id == R.id.Opciones) {
+                    selectedFragment = new FragmentOpciones();
                 }
 
-                if (selectedFragment!=null){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.BuscadorJugadorNombre,selectedFragment).commit();
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, selectedFragment).commit();
                 }
+
+                // Cierra el menú de navegación después de hacer una selección
+                drawerLayout.closeDrawer(GravityCompat.START);
+
                 return true;
             }
         });
-
     }
 
-    //cierre del menú con la pulsación del botón Atrás o back de Android.
+    // Controla el comportamiento del botón Atrás o back de Android.
     @Override
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -97,26 +89,4 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
-
-  /*  private int getTitle(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.BuscadorJugadorNombre:
-                return R.string.buscadorJugadorNombre;
-            case R.id.BuscadorClubesNombre:
-                return R.string.buscadorClubesNombre;
-            case R.id.BuscadorobjetosNombre:
-                return R.string.buscadorObjetosNombre;
-            case R.id.BuscadorSTNombre:
-                return R.string.buscadorSTNombre;
-            case R.id.ListaFavoritos:
-                return R.string.listaFavoritos;
-            case R.id.AcercaDe:
-                return R.string.AcercaDe;
-            case R.id.Opciones:
-                return R.string.Opciones;
-            default:
-                throw new IllegalArgumentException("menu option not implemented!!");
-        }
-    }*/
-
 }
