6e0b35b21af36e9031a7936d54ee440ecfbf9038
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2023-10-23 14:09:42 +0200

    Avance con el modo claro/oscuro: ya no parpadea la pantalla entera, solo el switch, lo dejo por ahí de momento

--- android/InazumaWiki/app/src/main/java/com/afundacion/inazumawiki/opciones/FragmentOpciones.java
+++ android/InazumaWiki/app/src/main/java/com/afundacion/inazumawiki/opciones/FragmentOpciones.java
@@ -1,78 +1,45 @@
 package com.afundacion.inazumawiki.opciones;
 
 import android.os.Bundle;
-
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.fragment.app.Fragment;
-
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
-import com.afundacion.myaplication.R;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
 
+import com.afundacion.myaplication.R;
 
 public class FragmentOpciones extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentOpciones() {
-        // Required empty public constructor
-    }
-
-
-    // TODO: Rename and change types and number of parameters
-    public static FragmentOpciones newInstance(String param1, String param2) {
-        FragmentOpciones fragment = new FragmentOpciones();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Switch switchNightMode;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-
-
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    Switch switchNightMode;
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_opciones, container, false);
 
         // Inicializa el Switch y establece su estado según las preferencias.
         switchNightMode = view.findViewById(R.id.ModoOscuro);
 
-
         // Agrega un Listener para manejar el cambio de estado del Switch.
         switchNightMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+            public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
                 // Actualiza el tema de la aplicación en tiempo real.
-                if (isChecked) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                }
-                recreateActivity();
+                int nightMode = isChecked ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO;
+                AppCompatDelegate.setDefaultNightMode(nightMode);
+
+                // Retrasa la recreación de la actividad para evitar problemas de parpadeo.
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        recreateActivity();
+                    }
+                }, 100);
             }
         });
 
@@ -84,7 +51,6 @@ public class FragmentOpciones extends Fragment {
             getActivity().recreate();
         }
     }
-
-
 }
+
 
