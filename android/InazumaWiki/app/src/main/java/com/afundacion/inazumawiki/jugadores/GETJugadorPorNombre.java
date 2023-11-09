3873eb4e2c189151a2e8f6fb515e87c8c066421b
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2023-11-09 14:19:52 +0100

    Version 0.1

--- android/InazumaWiki/app/src/main/java/com/afundacion/inazumawiki/jugadores/GETJugadorPorNombre.java
+++ android/InazumaWiki/app/src/main/java/com/afundacion/inazumawiki/jugadores/GETJugadorPorNombre.java
@@ -1,6 +1,5 @@
 package com.afundacion.inazumawiki.jugadores;
 
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
--- android/InazumaWiki/app/src/main/java/com/afundacion/inazumawiki/register/RegisterActivity.java
+++ android/InazumaWiki/app/src/main/java/com/afundacion/inazumawiki/register/RegisterActivity.java
@@ -3,7 +3,6 @@ package com.afundacion.inazumawiki.register;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
